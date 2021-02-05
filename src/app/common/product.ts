01960a5fe33c23ddcce8ea2d445e142a259e513c
neetesh24 <tamrakarnitesh21@gmail.com>
2021-02-05 11:25:34 +0545

    Creating the Angular front end for eshop project

---
+++ src/app/common/product.ts
@@ -0,0 +1,10 @@
+export class Product {
+    //this is a basic class to add property 
+    name :String;
+    description:String;
+    unitPrice:number;
+    imageUrl:String;
+    unitsInStock:number;
+    dateCreated:Date;
+    lastUpdate:Date
+}
